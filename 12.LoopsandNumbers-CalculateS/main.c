bb5e32e81e1fbc2b431a1205a6c45f5d4255a8ad
Julianne <juliannebagsh@gmail.com>
2016-02-02 14:02:28 +0000

    AlgosandCoding12
    AlgosandCoding C 12.LoopsandNumbers-CalculateS

--- 12.LoopsandNumbers-CalculateS/main.c
+++ 12.LoopsandNumbers-CalculateS/main.c
@@ -1,15 +1,82 @@
 //
 //  main.c
-//  12.LoopsandNumbers-CalculateS
+//  CalculateS
 //
-//  Created by Julianne on 15/01/2016.
+//  Created by Julianne on 11/01/2016.
 //  Copyright (c) 2016 Julianne. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int sume = 0;
+    
+    for (int s = 1; s <= 100; s++)
+    {
+        sume = sume + s;
+    }
+    
+    printf("%i\n", sume);
+    
+    
+    
+    int sumf = 0;
+    
+    for (int s = -1; s >= -200; s--)
+    {
+        sumf = sumf + s;
+    }
+    
+    printf("%i\n", sumf);
+    
+    
+    
+    float sumg = 0;
+    
+    for (float s = 2.0; s <= 100.0; s++)
+    {
+        float i = 1 / s;
+        sumg = sumg + i;
+    }
+    
+    printf("%f\n", sumg);
+    
+    
+    
+    int sumh = 0;
+    
+    for (int s = 1; s <= 100; s++)
+    {
+        sumh = sumh + s*s;
+    }
+    
+    printf("%i\n", sumh);
+    
+    
+    
+    float sumi = 0;
+    
+    for (float s = 2.0; s <= 99.0; s++)
+    {
+        float f = 1 / (s * (s + 1));
+        sumi = sumi + f;
+    }
+    
+    printf("%f\n", sumi);
+    
+    
+    
+    float sumj = 0;
+    
+    for (float s = 3.0; s <= 95.0; s = s + 4)
+    {
+        float k = 1 / (s * (s + 2));
+        sumj = sumj + k;
+    }
+    
+    printf("%f\n", sumj);
+    
+    
     return 0;
 }
